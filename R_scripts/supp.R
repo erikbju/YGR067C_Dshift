a21f1c48260610854a27c1dfaeb1ee04915fba1b
erikbju <erik.bjurstrom51@gmail.com>
2026-03-03 18:11:26 +0100

    Updated for resubmission //E

--- R_scripts/supp.R
+++ R_scripts/supp.R
@@ -46,6 +46,203 @@ nicefy_gene_results <- function(input, genelist) {
   return(out_table)
 }
 
+btest <- function (x1,
+                   n1,
+                   x2,
+                   n2,
+                   alternative = c("two.sided", "less", "greater"),
+                   or = NULL,
+                   conf.int = FALSE,
+                   conf.level = 0.95,
+                   midp = FALSE,
+                   tsmethod = c("central", "minlike"),
+                   control = ucControl())
+{
+  alternative <- match.arg(alternative)
+  tsmethod <- match.arg(tsmethod)
+  if (midp) {
+    T <- function(X1, N1, X2, N2, delta0 = 1) {
+      m <- length(X1)
+      pval <- rep(NA, m)
+      for (i in 1:m) {
+        pval[i] <- exact2x2(
+          matrix(c(X2[i], N2 - X2[i], X1[i], N1 - X1[i]), 2, 2),
+          or = delta0,
+          conf.int = FALSE,
+          midp = TRUE,
+          alternative = "less"
+        )$p.value
+      }
+      pval
+    }
+  }
+  else if (alternative == "less") {
+    T <- function(X1, N1, X2, N2, delta0 = 1) {
+      m <- length(X1)
+      pval <- rep(NA, m)
+      for (i in 1:m) {
+        pval[i] <- exact2x2(
+          matrix(c(X2[i], N2 - X2[i], X1[i], N1 - X1[i]), 2, 2),
+          or = delta0,
+          conf.int = FALSE,
+          midp = FALSE,
+          alternative = "less"
+        )$p.value
+      }
+      pval
+    }
+  }
+  else if (alternative == "greater") {
+    T <- function(X1, N1, X2, N2, delta0 = 1) {
+      m <- length(X1)
+      pval <- rep(NA, m)
+      for (i in 1:m) {
+        pval[i] <- exact2x2(
+          matrix(c(X2[i], N2 - X2[i], X1[i], N1 - X1[i]), 2, 2),
+          or = delta0,
+          conf.int = FALSE,
+          midp = FALSE,
+          alternative = "greater"
+        )$p.value
+      }
+      1 - pval
+    }
+  }
+  else if (alternative == "two.sided" & tsmethod == "minlike") {
+    T <- function(X1, N1, X2, N2, delta0 = 1) {
+      m <- length(X1)
+      pval <- rep(NA, m)
+      for (i in 1:m) {
+        cat("\r",
+            paste0(
+              "Minlike: ",
+              i,
+              " out of ",
+              m,
+              " completed ",
+              "(",
+              round(i / m * 100, 0),
+              "%)."
+            ))
+        pval[i] <- fisher.test(
+          matrix(c(X2[i], N2 -
+                     X2[i], X1[i], N1 - X1[i]), 2, 2),
+          or = delta0,
+          conf.int = FALSE,
+          alternative = "two.sided"
+        )$p.value
+      }
+      1 - pval
+    }
+    out <- uncondExact2x2(
+      x1,
+      n1,
+      x2,
+      n2,
+      alternative = alternative,
+      nullparm = or,
+      parmtype = "oddsratio",
+      conf.int = conf.int,
+      conf.level = conf.level,
+      midp = midp,
+      method = "user",
+      tsmethod = "square",
+      control = control,
+      Tfunc = T
+    )
+    out$method <- "Boschloo's test"
+    return(out)
+  }
+  else if (alternative == "two.sided" & tsmethod == "central") {
+    conf.level <- 1 - (1 - conf.level) / 2
+    T <- function(X1, N1, X2, N2, delta0 = 1) {
+      m <- length(X1)
+      pval <- rep(NA, m)
+      for (i in 1:m) {
+        cat("\r", paste0("Less:", i, " out of ", m, " completed."))
+        pval[i] <- exact2x2(
+          matrix(c(X2[i], N2 - X2[i], X1[i], N1 - X1[i]), 2, 2),
+          or = delta0,
+          conf.int = FALSE,
+          midp = FALSE,
+          alternative = "less"
+        )$p.value
+      }
+      pval
+    }
+    outLess <- uncondExact2x2(
+      x1,
+      n1,
+      x2,
+      n2,
+      alternative = "less",
+      nullparm = or,
+      parmtype = "oddsratio",
+      conf.int = conf.int,
+      conf.level = conf.level,
+      midp = midp,
+      method = "user",
+      control = control,
+      Tfunc = T
+    )
+    T <- function(X1, N1, X2, N2, delta0 = 1) {
+      m <- length(X1)
+      pval <- rep(NA, m)
+      for (i in 1:m) {
+        cat("\r", paste0("Greater:", i, " out of ", m, " completed."))
+        pval[i] <- exact2x2(
+          matrix(c(X2[i], N2 - X2[i], X1[i], N1 - X1[i]), 2, 2),
+          or = delta0,
+          conf.int = FALSE,
+          midp = FALSE,
+          alternative = "greater"
+        )$p.value
+      }
+      1 - pval
+    }
+    outGreater <- uncondExact2x2(
+      x1,
+      n1,
+      x2,
+      n2,
+      alternative = "greater",
+      nullparm = or,
+      parmtype = "oddsratio",
+      conf.int = conf.int,
+      conf.level = conf.level,
+      midp = midp,
+      method = "user",
+      control = control,
+      Tfunc = T
+    )
+    out <- outLess
+    out$p.value <- min(c(1, 2 * outLess$p.value, 2 * outGreater$p.value))
+    out$conf.int <- c(outGreater$conf.int[1], outLess$conf.int[2])
+    out$method <- "Boschloo's test"
+    return(out)
+  }
+  uncondMethod <- "user"
+  out <- uncondExact2x2(
+    x1,
+    n1,
+    x2,
+    n2,
+    alternative = alternative,
+    nullparm = or,
+    parmtype = "oddsratio",
+    conf.int = conf.int,
+    conf.level = conf.level,
+    midp = midp,
+    method = uncondMethod,
+    control = control,
+    Tfunc = T
+  )
+  out$method <- "Boschloo's test"
+  if (tsmethod == "central" & alternative == "two.sided")
+    out$method <- "Central Boschloo's test"
+  out
+}
+
 megatest <- function(reslist, GS, experiments = 1, seed = 819230) {
   set.seed(seed)
   gup <- list()
